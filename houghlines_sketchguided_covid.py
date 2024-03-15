c07b84d88decd394995b1aa6df1473e169cc4325
pedrosergiot <pedrosergiot@gmail.com>
2024-03-15 15:51:30 -0300

    first commit

---
+++ houghlines_sketchguided_covid.py
@@ -0,0 +1,191 @@
+import os
+import sys
+import numpy as np
+import tensorflow as tf
+
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import ModelCheckpoint
+from sklearn.model_selection import KFold
+
+from joblib import load
+from sketchguidedFuncs import *
+
+class_trained = sys.argv[1]
+
+num_folds = 10
+kfold_shuffle = False
+test_fold = int(sys.argv[2])
+
+images_path = '../ultrasound_images/croped_images_gray/'
+hough_path = '../images_edges_detection/PATH_TO_HOUGH_LINES/'
+current_path = './houghlines_sketchguided/kfold10/Fold1/'
+
+
+# define the discriminator
+def define_discriminator(in_shape=(256,256,1)):
+  # weight initialization
+  init = tf.random_normal_initializer(0., 0.02)
+  # source image input (Hough lines)
+  in_src_image = tf.keras.layers.Input(shape=in_shape)
+  # target image input (original imagem from which the lines are extracted)
+  in_target_image = tf.keras.layers.Input(shape=in_shape)
+  # concatenating both inputs
+  merged = tf.keras.layers.Concatenate()([in_src_image, in_target_image])
+  # 112
+  d = tf.keras.layers.Conv2D(64, (4,4), strides=(2,2), padding='same', kernel_initializer=init)(merged)
+  d = tf.keras.layers.LeakyReLU(alpha=0.2)(d)
+  # 56
+  d = tf.keras.layers.Conv2D(128, (4,4), strides=(2,2), padding='same', kernel_initializer=init)(d)
+  d = tf.keras.layers.BatchNormalization()(d)
+  d = tf.keras.layers.LeakyReLU(alpha=0.2)(d)
+  # 28
+  d = tf.keras.layers.Conv2D(256, (4,4), strides=(2,2), padding='same', kernel_initializer=init)(d)
+  d = tf.keras.layers.BatchNormalization()(d)
+  d = tf.keras.layers.LeakyReLU(alpha=0.2)(d)
+  # 14
+  d = tf.keras.layers.Conv2D(512, (4,4), strides=(2,2), padding='same', kernel_initializer=init)(d)
+  d = tf.keras.layers.BatchNormalization()(d)
+  d = tf.keras.layers.LeakyReLU(alpha=0.2)(d)
+  # second last output layer
+  d = tf.keras.layers.Conv2D(512, (4,4), padding='same', kernel_initializer=init)(d)
+  d = tf.keras.layers.BatchNormalization()(d)
+  d = tf.keras.layers.LeakyReLU(alpha=0.2)(d)
+  # patch output
+  d = tf.keras.layers.Conv2D(1, (4,4), padding='same', kernel_initializer=init)(d)
+  patch_out = tf.keras.layers.Activation('sigmoid')(d)
+  # define model
+  model = tf.keras.Model([in_src_image, in_target_image], patch_out, name='discriminator')
+  
+  return model
+
+
+# define the standalone generator model
+def define_generator(in_shape=(256,256,1)):
+  # weight initialization
+  init = tf.random_normal_initializer(0., 0.02)
+  # define model
+  in_image = tf.keras.layers.Input(shape=in_shape)
+  # encoder model: C64-C128-C256
+  e1 = encoder_block(in_image, 64, strides=2, batchnorm=False)
+  e2 = encoder_block(e1, 128, strides=2)
+  e3 = encoder_block(e2, 256, strides=2)
+  # bottleneck, no batch norm and relu
+  # residual blocks (10 or 15 of those)
+  b = residual_block(e3, filters=256, kernel_size=(3,3))
+  b = residual_block(b, filters=256, kernel_size=(3,3))
+  b = residual_block(b, filters=256, kernel_size=(3,3))
+  b = residual_block(b, filters=256, kernel_size=(3,3))
+  b = residual_block(b, filters=256, kernel_size=(3,3))
+  b = residual_block(b, filters=256, kernel_size=(3,3))
+  b = residual_block(b, filters=256, kernel_size=(3,3))
+  b = residual_block(b, filters=256, kernel_size=(3,3))
+  b = residual_block(b, filters=256, kernel_size=(3,3))
+  b = residual_block(b, filters=256, kernel_size=(3,3))
+  b = residual_block(b, filters=256, kernel_size=(3,3))
+  b = residual_block(b, filters=256, kernel_size=(3,3))
+  b = residual_block(b, filters=256, kernel_size=(3,3))
+  b = residual_block(b, filters=256, kernel_size=(3,3))
+  b = residual_block(b, filters=256, kernel_size=(3,3))
+  # decoder model: C256-C128-C64
+  d5 = decoder_block(b, 256, strides=2, dropout=False)
+  d6 = decoder_block(d5, 128, strides=2, dropout=False)
+  d7 = decoder_block(d6, 64, strides=2, dropout=False)
+  # output
+  g = tf.keras.layers.Conv2DTranspose(1, (3,3), strides=(1,1), padding='same', kernel_initializer=init)(d7)
+  out_image = tf.keras.layers.Activation('tanh')(g)
+  # define model
+  model = tf.keras.Model(in_image, out_image, name='generator')
+
+  return model
+
+
+real_images = load(images_path + 'images_' + class_trained)['images']
+hough_images = load(hough_path + 'isfcanny5_houghlines_edges_' + class_trained)
+
+real_images = np.expand_dims(real_images, axis=-1)  # all channels are equal since images are in grayscale
+hough_images = np.expand_dims(hough_images, axis=-1)
+
+real_images = (real_images - 127.5)/127.5
+hough_images = (hough_images - 127.5)/127.5
+
+
+# Separating fold K-Fold
+kf = KFold(n_splits=num_folds, shuffle=kfold_shuffle)
+
+folds_ind = np.array([test_index for _, test_index in kf.split(real_images)])
+
+test_real_images = real_images[folds_ind[test_fold]]
+test_hough_images = hough_images[folds_ind[test_fold]]
+
+if test_fold == 9:
+  val_fold = 0
+else:
+  val_fold = test_fold + 1
+  
+val_real_images = real_images[folds_ind[val_fold]]
+val_hough_images = hough_images[folds_ind[val_fold]]
+
+train_real_images = real_images[np.concatenate(np.delete(folds_ind, [test_fold, val_fold]))]
+train_hough_images = hough_images[np.concatenate(np.delete(folds_ind, [test_fold, val_fold]))]
+
+
+gen_img_path = current_path + class_trained + '/generated_images/'
+
+# MODEL PARAMETERS
+IMG_SHAPE = (256, 256, 1)
+BATCH_SIZE = 8
+
+# Set the number of epochs for trainining.
+epochs = 1000
+
+# Defining optimizers
+generator_optimizer = Adam(learning_rate=0.001, beta_1=0.5, beta_2=0.999)
+discriminator_optimizer = Adam(learning_rate=0.0001, beta_1=0.5, beta_2=0.999)
+
+discriminator = define_discriminator(in_shape=IMG_SHAPE)
+generator = define_generator(in_shape=IMG_SHAPE)
+
+# Instantiate the WGAN model.
+sketchguided = SketchGuided(
+    discriminator=discriminator,
+    generator=generator
+    )
+
+# Compile the WGAN model.
+sketchguided.compile(
+    d_optimizer=discriminator_optimizer,
+    g_optimizer=generator_optimizer,
+    g_loss_fn=generator_loss,
+    d_loss_fn=discriminator_loss,
+)
+
+
+# Instantiate the customer `GANMonitor` Keras callback.
+cbk = GANMonitor(val_hough_images, val_real_images, num_img=6, gen_img_path=gen_img_path)
+
+
+# Callbacks to save best model and history
+checkpoint_path = current_path + class_trained + '/checkpoint/'
+if not os.path.exists(checkpoint_path):
+    os.makedirs(checkpoint_path)
+
+STEPS_PER_EPOCH = train_real_images.shape[0]/BATCH_SIZE
+checkpoint_cbk = ModelCheckpoint(filepath=checkpoint_path + 'epoch-{epoch:04d}.ckpt',
+                                 monitor='d_loss',
+                                 save_freq=int(100*STEPS_PER_EPOCH),
+                                 mode='max',
+                                 save_weights_only=True,
+                                 verbose=1)
+
+logfilename = current_path + class_trained + '/history.csv'
+history_cbk = MyCSVLogger(logfilename)
+
+
+# Start training the model.
+#sketchguided.load_weights(current_path + class_trained + '/checkpoint/epoch-0398.ckpt')
+history = sketchguided.fit(train_hough_images, train_real_images,
+                      validation_data=(val_hough_images, val_real_images),
+                      batch_size=BATCH_SIZE, epochs=epochs,
+                      callbacks=[cbk, checkpoint_cbk, history_cbk],
+                      #initial_epoch=398)
+                      )
---
+++ sketchguidedFuncs.py
@@ -0,0 +1,184 @@
+__all__ = ["SketchGuided", "MyCSVLogger", "GANMonitor", 
+           "generator_loss", "discriminator_loss",
+           "residual_block", "encoder_block", "decoder_block"]
+
+import os
+import csv
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+
+
+class SketchGuided(tf.keras.Model):
+  def __init__(self,
+                discriminator,
+                generator
+                ):
+
+    super(SketchGuided, self).__init__()
+    self.discriminator = discriminator
+    self.generator = generator
+
+  def compile(self, d_optimizer, g_optimizer, d_loss_fn, g_loss_fn):
+    super(SketchGuided, self).compile()
+    self.d_optimizer = d_optimizer
+    self.g_optimizer = g_optimizer
+    self.d_loss_fn = d_loss_fn
+    self.g_loss_fn = g_loss_fn
+
+
+  #def train_step(self, real_images):
+  def train_step(self, data):
+    input_image, target = data
+
+    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+      # Generate fake images
+      gen_output = self.generator(input_image, training=True)
+      # Get the logits for the fake images
+      disc_generated_output = self.discriminator([input_image, gen_output], training=True)
+      # Get the logits for the real images
+      disc_real_output = self.discriminator([input_image, target], training=True)
+
+      gen_total_loss, gen_gan_loss, gen_l1_loss = self.g_loss_fn(disc_generated_output, gen_output, target)
+      disc_loss = self.d_loss_fn(disc_real_output, disc_generated_output)
+
+    # Calculate the gradients for generator and discriminator
+    generator_gradients = gen_tape.gradient(gen_total_loss, self.generator.trainable_variables)
+    discriminator_gradients = disc_tape.gradient(disc_loss, self.discriminator.trainable_variables)
+
+    # Apply the gradientes to the optimizer
+    self.g_optimizer.apply_gradients(zip(generator_gradients, self.generator.trainable_variables))
+    self.d_optimizer.apply_gradients(zip(discriminator_gradients, self.discriminator.trainable_variables))
+
+    return {"disc_loss": disc_loss, "gen_total_loss": gen_total_loss, "gen_l1_loss": gen_l1_loss}
+
+
+  def test_step(self, data):
+    input_image, target = data
+    generated_images = self.generator(input_image)
+    # Get the logits for generated images
+    disc_generated_output = self.discriminator([input_image, generated_images])
+    # Get the logits for the real images
+    disc_real_output = self.discriminator([input_image, target])
+
+    disc_loss = self.d_loss_fn(disc_real_output, disc_generated_output)
+
+    return {"disc_loss": disc_loss}
+
+
+# Keras callback to periodically save generated images
+class GANMonitor(tf.keras.callbacks.Callback):
+  def __init__(self, houghs, targets, num_img=6, gen_img_path='generated_images/'):
+    self.houghs = houghs
+    self.targets = targets
+    self.num_img = num_img
+    self.gen_img_path = gen_img_path
+
+  def on_epoch_end(self, epoch, logs=None):
+    random_inds = np.random.randint(0, tf.shape(self.houghs)[0], self.num_img)
+
+    generated_images = self.model.generator(self.houghs[random_inds])
+    real_images = self.targets[random_inds]
+
+    generated_images = (generated_images * 127.5) + 127.5
+    real_images = (real_images * 127.5) + 127.5
+
+    for i in range(self.num_img):
+      img = generated_images[i].numpy()
+      img = tf.keras.preprocessing.image.array_to_img(img)
+      path_img = self.gen_img_path + "img_{i}_{epoch}_generated.png".format(i=i, epoch=epoch)
+      dir_img = os.path.dirname(path_img)
+      if not os.path.exists(dir_img):
+          os.makedirs(dir_img)
+      img.save(path_img)
+
+      img = (self.targets[random_inds])[i]
+      img = tf.keras.preprocessing.image.array_to_img(img)
+      path_img = self.gen_img_path + "img_{i}_{epoch}_real.png".format(i=i, epoch=epoch)
+      img.save(path_img)
+
+
+class MyCSVLogger(tf.keras.callbacks.Callback):
+
+  def __init__(self, filename):
+    self.filename = tf.compat.path_to_str(filename)
+
+  def on_train_begin(self, epoch, logs=None):
+    if not os.path.exists(self.filename):
+      self.csv_file = open(self.filename, 'a')
+      self.writer = csv.writer(self.csv_file)
+      self.writer.writerow(['disc_loss', 'gen_total_loss', 'gen_l1_loss', 'val_disc_loss'])
+      self.csv_file.close()
+
+  def on_epoch_end(self, epoch, logs=None):
+    pd.DataFrame(logs, index=[epoch]).to_csv(self.filename, mode = 'a', header=False, index=False)
+
+
+
+def generator_loss(disc_generated_output, gen_output, target, LAMBDA=100):
+  loss_object = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
+  gan_loss = loss_object(tf.ones_like(disc_generated_output), disc_generated_output)
+
+  # Mean absolute error
+  l1_loss = tf.reduce_mean(tf.abs(target - gen_output))
+  total_gen_loss = gan_loss + (LAMBDA * l1_loss)
+
+  return total_gen_loss, gan_loss, l1_loss
+
+
+def discriminator_loss(disc_real_output, disc_generated_output):
+  loss_object = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
+  real_loss = loss_object(tf.ones_like(disc_real_output), disc_real_output)
+  generated_loss = loss_object(tf.zeros_like(disc_generated_output), disc_generated_output)
+
+  total_disc_loss = real_loss + generated_loss
+
+  return total_disc_loss
+
+
+def residual_block(x: tf.Tensor, filters: int, kernel_size: int = 3) -> tf.Tensor:
+	y = tf.keras.layers.Conv2D(filters=filters, kernel_size=kernel_size, strides=1, padding="same")(x)
+	y = tf.keras.layers.ReLU()(y)
+	y = tf.keras.layers.BatchNormalization()(y)
+ 
+	y = tf.keras.layers.Conv2D(filters=filters, kernel_size=kernel_size, strides=1, padding="same")(y)
+	out = tf.keras.layers.Add()([x, y]) 
+	out = tf.keras.layers.ReLU()(out)
+	out = tf.keras.layers.BatchNormalization()(out)
+	return out
+
+
+# define an encoder block
+def encoder_block(layer_in, n_filters, filter=3, strides=2, batchnorm=True, dropout=False):
+	# weight initialization
+	init = tf.random_normal_initializer(0., 0.02)
+	# add downsampling layer
+	g = tf.keras.layers.Conv2D(n_filters, filter, strides=strides, padding='same', kernel_initializer=init)(layer_in)
+	# conditionally add batch normalization
+	if batchnorm:
+		g = tf.keras.layers.BatchNormalization()(g, training=True)
+	# leaky relu activation
+	if dropout:
+		g = tf.keras.layers.Dropout(0.5)(g, training=True)
+	# relu activation
+	g = tf.keras.layers.ReLU()(g)
+	return g
+
+
+# define a decoder block
+def decoder_block(layer_in, n_filters, filter=3, strides=2, batchnorm=True, dropout=False):
+	# weight initialization
+	init = tf.random_normal_initializer(0., 0.02)
+	# add upsampling layer
+	g = tf.keras.layers.Conv2DTranspose(n_filters, (3,3), strides=(2,2), padding='same', kernel_initializer=init)(layer_in)
+	# add batch normalization
+	if batchnorm:
+		g = tf.keras.layers.BatchNormalization()(g, training=True)
+	# conditionally add dropout
+	if dropout:
+		g = tf.keras.layers.Dropout(0.5)(g, training=True)
+	# relu activation
+	g = tf.keras.layers.ReLU()(g)
+	return g
